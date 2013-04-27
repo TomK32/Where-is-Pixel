81d4d1c518cb97ce9a70f7cef905fb521f8f1d40
Thomas R. Koll <tomk32@tomk32.de>
2013-04-27 18:20:50 +0200

    Use anonymous classes in levels and make it a nice red flag for the south pole

--- levels/001-southpole.lua
+++ levels/001-southpole.lua
@@ -6,7 +6,7 @@ return {
   description = 'Can you find him here in this white desert?',
   pixel_size = 4,
   entities = {
-    background = class("PoleBackground", Background):include({
+    background = Background:subclass():include({
       drawPixel = function(self, x, y)
         local p = self.level.pixel_size
         local c = math.ceil(SimplexNoise.Noise2D(math.floor(x / p), math.floor( y / p)) * 20)
@@ -15,13 +15,16 @@ return {
         love.graphics.rectangle('fill', x * p, y * p, p, p)
       end
     }),
-    pole = class("Pole", Entity):include({
+    pole = Entity:subclass():include({
       onInitialize = function(self)
         self.position = { x = self.level.width / 2, y = self.level.height / 2, z = 5 }
       end,
       drawContent = function(self)
+        local p = self.level.pixel_size
         love.graphics.setColor( 0, 0, 0, 255)
-        love.graphics.rectangle('fill', 0, 0, self.level.pixel_size, self.level.pixel_size * 2)
+        love.graphics.rectangle('fill', 0, 0, p/2, p * 4)
+        love.graphics.setColor( 255, 0, 0, 255)
+        love.graphics.rectangle('fill', 0, p/2, p * 2, p)
       end
     })
   },
